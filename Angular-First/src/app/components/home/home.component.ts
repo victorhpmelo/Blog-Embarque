07cebc3517c2145568b6b6bef4a88172e6cb6656
victorhpmelo <victorhpmelo01@hotmail.com>
2024-09-21 19:16:57 -0300

    Segundo commit de preparação completa

--- Angular-First/src/app/components/home/home.component.ts
+++ Angular-First/src/app/components/home/home.component.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { FooterModule } from '../footer/footer.module';
+import { CommonModule } from '@angular/common';
+import { HelloWorldService } from '../../services/hello-world.service';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-// importando o modulo para usar o componente
-  imports: [FooterModule],
+// importando modulos para usar os componentes
+  imports: [FooterModule,CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
@@ -15,14 +17,37 @@ export class HomeComponent {
   randomUrl = "Inserir url Aqui";
   cont = signal(0);
   name1 = signal("Oi ")
+  value = true
+
 // Definindo o metodo
   teste(){
     alert("Oi");
     }
 
-// Usando Signals pro valor ser atualizado quando a funcao do botao for chamada
+// Usando Signals pro valor ser atualizado de forma dinamica quando a funcao do botao for chamada
     contagem(){
   this.cont.update(cont => cont + 1);
   this.name1.set("Oi " + this.cont());
     }
+
+  lista = [
+    //"Victor","Gustavo","Daniel","Adrielly","Felipe"
+  ]
+
+  // Pegando os dados da chamada
+  constructor(private service:HelloWorldService ){
+    this.service.getHelloWorld().subscribe(
+      {
+        next:(data)=>{
+          console.log(data)
+        },
+        error:(error)=> {
+          console.log(error)
+        },
+        complete:()=>[
+          console.log("Finalizou")
+        ]
+      }
+    )
+  }
 }
